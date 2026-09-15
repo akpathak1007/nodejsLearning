9d3e9d96634c0c7701d1418b82beaffaab660654
Your Name <you@example.com>
2026-09-15 12:29:33 +0530

    math logics

--- dsa/dsa.mathLogics.js
+++ dsa/dsa.mathLogics.js
@@ -1,12 +1,155 @@
 const visibility = isVisible();
 
-practice(visibility.allFactorsEfficientAlgo, () => {});
+practice(visibility.arrayAsFrequncyTable, () => {
+  // Array as frequecy table when input is reasonally small
+  // constrant 0<= value <=100
+  const arr = [2, 4, 2, 1, 4, 2, 5];
+  const handler = (arr) => {
+    // Right now frequecy of all value is 0
+    // here index is a value
+    const arrayFrequencyTable = Array(101).fill(0);
+    for (let i = 0; i < arr.length; i++) {
+      arrayFrequencyTable[arr[i]]++;
+    }
+    let mostFrequency = 0;
+    let mostFrequent = 0;
+    for (let j = 0; j < arrayFrequencyTable.length; j++) {
+      let value = arrayFrequencyTable[j];
+      if (mostFrequency < value) {
+        console.log(mostFrequency, mostFrequent);
+        mostFrequency = value;
+        mostFrequent = j;
+      }
+    }
+    return {
+      mostFrequency,
+      mostFrequent,
+    };
+  };
+  // Time -> O(n+k), O(k)
+  // k= value range / frequecy table
+  // n = number of elements in input
+  console.log(handler(arr));
+});
+
+practice(visibility.firstRepeatingElement, () => {
+  const arr = [10, 5, 3, 4, 3, 5, 6];
+  const handler = (arr) => {
+    const seen = new Set();
+    for (let i of arr) {
+      if (seen.has(i)) {
+        return i;
+      } else {
+        seen.add(i);
+      }
+    }
+    return null;
+  };
+  // Time O(n) Space O(n)
+  console.log(handler(arr));
+  console.log(handler([1, 2, 3, 4, 5]));
+  console.log(handler([2, 2, 2, 2, 2, 2]));
+  console.log(handler([1, 2, 3, 4, 5, 6, 7, 7]));
+  console.log(handler([1, 2, 3, 4, 5, 6, 7, 1]));
+});
+
+practice(visibility.groupAnagrams, () => {
+  const arr = ["eat", "tea", "tan", "ate", "nat", "bat"];
+  const handler = (arr) => {
+    const group = new Map();
+    for (let i of arr) {
+      const key = i.split("").sort().join("");
+      if (group.has(key)) {
+        // Gives referect to array so we can modify
+        //it directly without push
+        group.get(key).push(i);
+      } else {
+        group.set(key, [i]);
+      }
+    }
+    return group;
+  };
+  console.log(handler(arr));
+  /**  Split O(k), sort O(k logk), join O(k) Map O(1) Average
+       Complexity -> time O(n*k log k), space O(n*k)
+  */
+});
+
+// ## Solution with Map + Frequecy
+practice(visibility.firstUniqueWIthMap, () => {
+  console.log("$$$ firstUniqueWithMap");
+  const arr1 = [4, 5, 1, 2, 1, 4, 5];
+  function firstUnique(arr) {
+    let seen = new Map();
+    for (let i of arr) {
+      seen.set(i, (seen.get(i) ?? 0) + 1);
+    }
+    for (let [x, y] of seen) {
+      if (y === 1) {
+        return x;
+      }
+    }
+    return null;
+  }
+
+  console.log(firstUnique(arr1));
+  console.log(firstUnique([2, 3, 2, 4, 1, 5]));
+  console.log(firstUnique([1, 2, 3, 4]));
+  console.log(firstUnique([1, 1]));
+  console.log(firstUnique([1, 2, 3, 4, 5, 6, 7, 8, 1]));
+});
+
+// ## Solution with one seen Set and unique
+// with their insertions order and delete if duplicate found
+practice(visibility.firstUniqueWithSet, () => {
+  const arr1 = [4, 5, 1, 2, 1, 4, 5];
+  function firstUnique(arr) {
+    let unique = new Set();
+    let seen = new Set();
+    for (let i of arr) {
+      if (!seen.has(i)) {
+        unique.add(i);
+      } else {
+        unique.delete(i);
+      }
+      seen.add(i);
+    }
+    return unique.values().next().value ?? null;
+  }
+
+  console.log(firstUnique(arr1));
+  console.log(firstUnique([2, 3, 2, 4, 1, 5]));
+  console.log(firstUnique([1, 2, 3, 4]));
+  console.log(firstUnique([1, 1]));
+  console.log(firstUnique([1, 2, 3, 4, 5, 6, 7, 8, 1]));
+});
+practice(visibility.allFactorsEfficientAlgo, () => {
+  // More efficient and correct solution than before
+  const handler = (input) => {
+    let i = 1;
+    const factors = [];
+    while (i * i <= input) {
+      const check = input % i === 0;
+      if (check) {
+        factors.push(i);
+        if (i !== input / i) {
+          factors.push(input / i);
+        }
+      }
+      i++;
+    }
+    return factors;
+  };
+  console.log(handler(36));
+  console.log(handler(0));
+  console.log(handler(724807033));
+});
 
 practice(visibility.allFactorsByBruteForce, () => {
   // Finding all factors by brute force
-  const n = 366;
+  const n = 7248070331;
   let i = 1;
-  while (i <= 366) {
+  while (i <= n) {
     if (n % i === 0) {
       console.log(i);
     }
@@ -194,7 +337,12 @@ function isVisible(name) {
     armsStrongCorrectLogic: false,
     findingAllFactors: false,
     allFactorsByBruteForce: false,
-    allFactorsEfficientAlgo: true,
+    allFactorsEfficientAlgo: false,
+    firstUniqueWithSet: false,
+    firstUniqueWIthMap: false,
+    groupAnagrams: false,
+    firstRepeatingElement: false,
+    arrayAsFrequncyTable: true,
   };
 
   return testFunction;
